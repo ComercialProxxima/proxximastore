d2e61d3a800f528ac647474975c7a1f41478d40b
ComercialProxxima <anderson.silva.proxxima@gmail.com>
2025-05-15 14:06:47 -0300

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -11,5 +11,10 @@ if (!process.env.DATABASE_URL) {
   );
 }
 
-export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // Para ambiente de desenvolvimento
+  }
+});
 export const db = drizzle({ client: pool, schema });
